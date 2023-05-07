97e376701b68fa32b25c4933db4ed53165708bca
gongbenzheng <1459490255@qq.com>
2023-05-07 15:20:50 +0800

    vue3-去掉多余的路由

--- src/views/SentinelView.vue
+++ src/views/SentinelView.vue
@@ -1,14 +1,4 @@
 <template>
-  <nav>
-    <router-link to="/">Login</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/table">Table</router-link> |
-    <router-link to="/tablePost">TablePost</router-link> |
-    <router-link to="/home">Home</router-link> |
-    <router-link to="/sentinel">OtherViews</router-link> |
-    <router-link to="/template">Template</router-link>
-  </nav>
-  <router-view/>
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
